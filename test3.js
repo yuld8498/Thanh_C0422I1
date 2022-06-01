4c383923be03a20fdb116eeb84354608c615cc8c
yuld8498 <truongthanh11201@gmail.com>
2022-06-01 10:17:47 +0700

    add

--- test3.js
+++ test3.js
@@ -1,7 +1,12 @@
 let text = "Hello Worlds!"
 let result =0;
 for(let i=0;i<text.length;i++){
-    text[i]=="a"?result+=1: text[i]=="o"? result+=1: text[i]=="e"? result+=1:  text[i]=="u"? result+=1: text[i]=="i"?result+=1: result = result;
+    text[i] == "a" ? result += 1
+        : text[i] == "o" ? result += 1
+            : text[i] == "e" ? result += 1 
+                :  text[i] == "u" ? result += 1
+                    : text[i] == "i" ? result += 1 
+                        : result = result;
 }
 if (result==0) {
     console.log("Chuỗi vừa nhập không có kí tự nguyên âm nào");
