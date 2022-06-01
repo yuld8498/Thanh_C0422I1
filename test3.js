7bfbed3058099c3cde3addf956e880213f8108e1
yuld8498 <truongthanh11201@gmail.com>
2022-06-01 10:38:22 +0700

    Add

--- test3.js
+++ test3.js
@@ -1,17 +1,18 @@
-let text = "Hello Worlds!"
+let text = "HEllo Worlds!"
 let result =0;
 function vowel(){
     for(let i=0;i<text.length;i++){
-        text[i] == "a" ? result += 1
-            : text[i] == "o" ? result += 1
-                : text[i] == "e" ? result += 1 
-                    :  text[i] == "u" ? result += 1
-                        : text[i] == "i" ? result += 1 
+        text[i] == "a"|| text[i] == "A" ? result += 1
+            : text[i] == "o" || text[i] == "O" ? result += 1
+                : text[i] == "e"||text[i] == "E" ? result += 1 
+                    :  text[i] == "u"||text[i] == "U" ? result += 1
+                        : text[i] == "i"|| text[i] == "I" ? result += 1 
                             : result = result;
     }
     if (result==0) {
-        console.log("Chuỗi vừa nhập không có kí tự nguyên âm nào");
+        console.log(false);
     } else {
         console.log(`Số các kí tự nguyên âm là ${result}`);
     }
 }
+vowel();
